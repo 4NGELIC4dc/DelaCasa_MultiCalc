32fcdfd2c9ae8f4cd8082a71c1f30c9d1d22ec95
4NGELIC4dc <angelica020403@gmail.com>
2022-05-31 18:39:54 +0800

    landscape on main activity

--- DelaCasa_TurnBasedGame/app/src/main/java/com/example/delacasa_turnbasedgame/MainActivity.java
+++ DelaCasa_TurnBasedGame/app/src/main/java/com/example/delacasa_turnbasedgame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.delacasa_turnbasedgame;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
 
         final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.start_sfx);
